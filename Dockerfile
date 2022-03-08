18333e333ffa15892bec9f5eff3595e94432d76d
Tomasz Malisz <tomasz.malisz@applica.ai>
2022-03-08 21:12:04 +0100

    Basic image preprocessing

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM python:latest
 RUN apt-get update -y
-RUN apt update && apt install -y libsm6 libxext6
+RUN apt-get -y install libsm6 libxext6 ffmpeg
 RUN apt-get -y install tesseract-ocr
 RUN apt-get -y install tesseract-ocr-pol
 RUN apt-get -y install tesseract-ocr-ukr
 RUN apt-get -y install tesseract-ocr-rus
 WORKDIR /code
-COPY ./app /code/app
 COPY requirements.txt .
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+COPY ./app /code/app
 RUN export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata/
 CMD ["uvicorn", "app.app:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/ocr_engine.py
+++ app/ocr_engine.py
@@ -1,12 +1,18 @@
 import pytesseract
 from typing import List
+import cv2
+import numpy as np
+from PIL import Image
 
 
 class OCREngine:
 
     @staticmethod
-    def get_text(image: bytes, language: str) -> str:
-        return pytesseract.image_to_string(image, lang=language)
+    def get_text(image: Image, language: str) -> str:
+        image = cv2.bilateralFilter(np.asarray(image), 5, 55, 60)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, image = cv2.threshold(image, 240, 255, 1)
+        return pytesseract.image_to_string(Image.fromarray(image), lang=language)
 
     @staticmethod
     def get_languages() -> List[str]:
