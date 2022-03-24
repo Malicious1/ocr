096d2659d5980d2c7cae476afb04f2671659bda2
Tomasz Malisz <tomasz.malisz@applica.ai>
2022-03-24 20:48:03 +0100

    Now we can look at the images preprocessing

--- evaluation/test_executor.py
+++ evaluation/test_executor.py
@@ -4,7 +4,6 @@ from typing import List
 from pydantic import BaseModel
 from PIL import Image
 import io
-from dataclasses import dataclass
 
 from app.ocr_engine import OCREngine
 from evaluation.evaluator import Evaluator, MetricResult
@@ -12,20 +11,18 @@ from evaluation.metrics import BuiltInMatcher, JaccardSimilarity
 
 
 class Annotation(BaseModel):
-
     file_name: str
     language: str
     text: str
 
 
-@dataclass
-class SampleEvaluationResults:
+class SampleEvaluationResults(BaseModel):
     file_name: str
     expected_text: str
     predicted_text: str
     metric_results: List[MetricResult]
 
-    def __repr__(self):
+    def __str__(self):
         metrics_text = ""
         for m in self.metric_results:
             metrics_text += str(m) + "\n"
@@ -36,17 +33,24 @@ class SampleEvaluationResults:
                metrics_text + "_" * 90
 
 
+class TestReport(BaseModel):
+    results_per_sample: List[SampleEvaluationResults]
+    summary: List[MetricResult]
+
+
 class TestExecutor:
 
-    def __init__(self, images_path: str, annotations_path: str, evaluator: Evaluator = None,
-                 postprocess_expected: bool = True, silent: bool = False):
+    def __init__(self, images_path: Path, annotations_path: Path, results_path: Path = None,
+                 preprocessd_images_path: Path = None, evaluator: Evaluator = None, postprocess_expected: bool = True,
+                 silent: bool = False):
         self.images_path = images_path
         self.annotations_path = annotations_path
+        self.results_path = results_path
+        self.preprocessed_images_path = preprocessd_images_path
         self.ocr_engine = OCREngine()
         self.silent = silent
         self.postprocess_expected = postprocess_expected
-        if evaluator is None:
-            self.evaluator = Evaluator(metrics=[JaccardSimilarity(), BuiltInMatcher()])
+        self.evaluator = Evaluator(metrics=[JaccardSimilarity(), BuiltInMatcher()]) if evaluator is None else evaluator
 
     def read_annotations(self) -> List[Annotation]:
         with open(self.annotations_path) as f:
@@ -58,13 +62,25 @@ class TestExecutor:
             image = Image.open(io.BytesIO(f.read()))
             return image
 
+    def save_preprocessed_image(self, image: Image, file_name: str):
+        with open(self.preprocessed_images_path / file_name, "wb") as f:
+            image.save(f)
+
+    def save_test_report(self, report: TestReport):
+        with open(self.results_path, "a") as f:
+            f.write(report.json())
+
     def evaluate_sample(self, annotation: Annotation) -> SampleEvaluationResults:
         image = self.read_image(annotation.file_name)
         text = self.ocr_engine.get_text(image, annotation.language)
         if self.postprocess_expected:
             annotation.text = self.ocr_engine.postprocess_text(annotation.text)
+        if self.preprocessed_images_path is not None:
+            preprocessed_image = self.ocr_engine.preprocess_image(image)
+            self.save_preprocessed_image(preprocessed_image, annotation.file_name)
         results = SampleEvaluationResults(
-            annotation.file_name, annotation.text, text, self.evaluator.evaluate(annotation.text, text))
+            file_name=annotation.file_name, expected_text=annotation.text, predicted_text=text,
+            metric_results=self.evaluator.evaluate(annotation.text, text))
         if not self.silent:
             print(results)
         return results
@@ -73,6 +89,11 @@ class TestExecutor:
         annotations = self.read_annotations()
         all_results = [self.evaluate_sample(a) for a in annotations]
         aggregated_results = self.evaluator.aggregate_results([r.metric_results for r in all_results])
+        report = TestReport(results_per_sample=all_results, summary=aggregated_results)
         if not self.silent:
-            print("\n" + "AGGREGATED: " + str(aggregated_results))
-        return all_results, aggregated_results
+            print("\n" + "AGGREGATED: ")
+            for m in aggregated_results:
+                print(m)
+        if self.results_path is not None:
+            self.save_test_report(report)
+        return report
--- tests/non_functional_tests/test_ocr_quality.py
+++ tests/non_functional_tests/test_ocr_quality.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--silent", help="Do not print results", action='store_true')
     parser.add_argument("--postprocess_expected", help="Should expected text be post-processed", action='store_true')
-    parser.add_argument("--annotations_path", help="path to jsonlines file with annotations")
-    parser.add_argument("--images_path", help="path to directory with images")
+    parser.add_argument("--annotations_path", help="path to jsonlines file with annotations", type=Path)
+    parser.add_argument("--images_path", help="path to directory with images", type=Path)
+    parser.add_argument("--results_path", help="path to jsonlines file where test report should be saved", type=Path)
+    parser.add_argument("--preprocessed_images_path", help="path to directory to save pre-processed images", type=Path)
     args = parser.parse_args()
 
     if args.images_path is None or args.annotations_path is None:
@@ -22,6 +24,9 @@ if __name__ == "__main__":
     runner = TestExecutor(
         annotations_path=args.annotations_path,
         images_path=args.images_path,
+        results_path=args.results_path,
+        preprocessd_images_path=args.preprocessed_images_path,
         silent=args.silent,
-        postprocess_expected=args.postprocess_expected)
+        postprocess_expected=args.postprocess_expected
+    )
     results = runner.run_test()
